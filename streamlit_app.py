1fd0071aa7e094fba01ee5d791fa99f257c735f4
Rafal Kuc <rafick997@gmail.com>
2022-10-21 10:24:09 +0200

    Update streamlit_app.py
    add pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 
 import streamlit
-
+import pandas
 streamlit.title("Parents cooking dinner")
 streamlit.header("Dinner menu")
 streamlit.text("Item 1")
 streamlit.text("Item 2")
 streamlit.text("Item 3")
 streamlit.text("Enjoy!")
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
