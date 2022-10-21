ca5541d9c38a61d420acd982efc91e115a015f67
Rafal Kuc <rafick997@gmail.com>
2022-10-21 10:15:46 +0200

    Update streamlit_app.py
    Complete menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 
 import streamlit
+
 streamlit.title("Parents cooking dinner")
+streamlit.header("Dinner menu")
+streamlit.text("Item 1")
+streamlit.text("Item 2")
+streamlit.text("Item 3")
+streamlit.text("Enjoy!")
